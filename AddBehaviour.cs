36eb2998ac179d4b30015dfe384b60233bdd207f
Tiwit27 <lojekrafal27@gmail.com>
2023-12-01 22:26:17 +0100

    Update AddBehaviour.cs

--- AddBehaviour.cs
+++ AddBehaviour.cs
@@ -12,9 +12,11 @@ public class AddBehaviour : MonoBehaviour
     public void OpenAddNewTab()
     {
         appBehaviour.addNew.SetActive(true);
-        form.day.text = System.DateTime.Now.ToString("dd");
-        form.month.text = System.DateTime.Now.ToString("MM");
-        form.year.text = System.DateTime.Now.ToString("yyyy");
+        form.day.text = System.DateTime.Now.Day.ToString();
+        form.month.text = System.DateTime.Now.Month.ToString();
+        form.year.text = System.DateTime.Now.Year.ToString();
+        form.lastRainDay.text = System.DateTime.Now.Day.ToString();
+        form.lastRainMonth.text = System.DateTime.Now.Month.ToString();
     }
     public void ChangeDate()
     {
@@ -24,65 +26,126 @@ public class AddBehaviour : MonoBehaviour
         //month
         if (month <= 0 || month > 12)
         {
-            form.month.text = System.DateTime.Now.ToString("MM");
+            form.month.text = System.DateTime.Now.Month.ToString();
         }
         //day
         if (month == 4 || month == 6 || month == 9 || month == 11)
         {
             if(day > 30 || day <= 0)
             {
-                form.day.text = System.DateTime.Now.ToString("dd");
+                form.day.text = System.DateTime.Now.Day.ToString();
             }
         }
         else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
         {
             if (day > 31 || day <= 0)
             {
-                form.day.text = System.DateTime.Now.ToString("dd");
+                form.day.text = System.DateTime.Now.Day.ToString();
             }
         }
         //year
-        if(year < 2010 || year > int.Parse(System.DateTime.Now.ToString("yyyy")))
+        if(year < 2010 || year > int.Parse(System.DateTime.Now.Year.ToString()))
         {
-            form.year.text = System.DateTime.Now.ToString("yyyy");
+            form.year.text = System.DateTime.Now.Year.ToString();
         }
     }
     public void ChangeTemperature()
     {
-        if(int.Parse(form.temperature.text) > 50 || int.Parse(form.temperature.text) < -20 || form.temperature.text == "-0" || form.temperature.text == "-00")
+        if (form.temperature.text == "-0" || form.temperature.text == "-00" || form.temperature.text == "-")
         {
             form.temperature.text = "";
+        }
+        else
+        {
+            if (int.Parse(form.temperature.text) > 50 || int.Parse(form.temperature.text) < -20)
+            {
+                form.temperature.text = "";
+            }
         }
     }
     public void ChangeLastRain()
     {
-        var month = int.Parse(form.lastRainMonth.text);
-        var day = int.Parse(form.lastRainDay.text);
-        //month
-        if (month <= 0 || month > 12)
+        if (form.lastRainMonth.text != "" && form.lastRainDay.text != "")
         {
-            form.lastRainMonth.text = System.DateTime.Now.ToString("MM");
-        }
-        //day
-        if (month == 4 || month == 6 || month == 9 || month == 11)
-        {
-            if (day > 30 || day <= 0)
+            var month = int.Parse(form.lastRainMonth.text);
+            var day = int.Parse(form.lastRainDay.text);
+            //month
+            if (month <= 0 || month > 12)
             {
-                form.lastRainDay.text = System.DateTime.Now.ToString("dd");
+                form.lastRainMonth.text = System.DateTime.Now.Month.ToString();
+            }
+            //day
+            if (month == 4 || month == 6 || month == 9 || month == 11)
+            {
+                if (day > 30 || day <= 0)
+                {
+                    form.lastRainDay.text = System.DateTime.Now.Day.ToString();
+                }
+            }
+            else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            {
+                if (day > 31 || day <= 0)
+                {
+                    form.lastRainDay.text = System.DateTime.Now.Day.ToString();
+                }
             }
         }
-        else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+        else
         {
-            if (day > 31 || day <= 0)
-            {
-                form.lastRainDay.text = System.DateTime.Now.ToString("dd");
-            }
+            form.lastRainDay.text = System.DateTime.Now.Day.ToString();
+            form.lastRainMonth.text = System.DateTime.Now.Month.ToString();
         }
     }
 
     public void Add()
     {
-        //Instantiate()
+        var color = new Color(0.4117647f, 0.6039216f, 0.5607843f);
+        if (form.lake.text.Length >= 2 && form.place.text.Length >= 5 && form.temperature.text.Length >= 1 && form.windValue.text.Length >= 1)
+        {
+            //clear
+            
+            form.lake.GetComponentInParent<Image>().color = color;
+            form.place.GetComponentInParent<Image>().color = color;
+            form.temperature.GetComponentInParent<Image>().color = color;
+            form.windValue.GetComponentInParent<Image>().color = color;
+            //wykonaj
+            Debug.Log("Wywo³aj");
+        }
+        else
+        {
+            if (form.lake.text.Length < 2)
+            {
+                form.lake.GetComponentInParent<Image>().color = new Color(0.6039216f, 0.5495574f, 0.4117647f);
+            }
+            else
+            {
+                form.lake.GetComponentInParent<Image>().color = color;
+            }
+            if (form.place.text.Length < 5)
+            {
+                form.place.GetComponentInParent<Image>().color = new Color(0.6039216f, 0.5495574f, 0.4117647f);
+            }
+            else
+            {
+                form.place.GetComponentInParent<Image>().color = color;
+            }
+            if (form.temperature.text.Length < 1)
+            {
+                form.temperature.GetComponentInParent<Image>().color = new Color(0.6039216f, 0.5495574f, 0.4117647f);
+            }
+            else
+            {
+                form.temperature.GetComponentInParent<Image>().color = color;
+            }
+            if (form.windValue.text.Length < 1)
+            {
+                form.windValue.GetComponentInParent<Image>().color = new Color(0.6039216f, 0.5495574f, 0.4117647f);
+            }
+            else
+            {
+                form.windValue.GetComponentInParent<Image>().color = color;
+            }
+        }
     }
 }
 [System.Serializable]
